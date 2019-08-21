99232cd3c72e059eab80b2954b2529a2b8eb9cc0
mfaizan1 <mfaizanulhaq1@gmail.com>
2019-08-21 17:24:45 +0500

    Explicit types

--- scripts.js
+++ scripts.js
@@ -1,13 +1,5 @@
-function Greeter(greeting) {
-    this.greeting = greeting;
-}
-Greeter.prototype.greet = function () {
-    return "Hello, " + this.greeting;
-};
-var greeter = new Greeter("world");
-var button = document.createElement("button");
-button.textContent = "Say Hello";
-button.onclick = function () {
-    alert(greeter.greet());
-};
-document.body.appendChild(button);
+"use strict";
+var myName = "faizan";
+var isMarried = false;
+var myAge;
+myAge = 24;
