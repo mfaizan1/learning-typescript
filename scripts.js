72a4572695ff97954626b3624864007c5bdef06e
mfaizan1 <mfaizanulhaq1@gmail.com>
2019-08-21 17:34:13 +0500

    Array expicit types

--- scripts.js
+++ scripts.js
@@ -3,3 +3,7 @@ var myName = "faizan";
 var isMarried = false;
 var myAge;
 myAge = 24;
+var fruits = ['apple', 'banana', 'mango'];
+fruits = ['cost', 10];
+var cars = ['Honda', 'Faw', 'BMW'];
+var numberArray = [1, 2, 3, 4];
--- scripts.ts
+++ scripts.ts
@@ -2,3 +2,8 @@ let myName: string = "faizan";
 let isMarried: boolean = false;
 let myAge : number;
 myAge = 24;
+
+let fruits : any[] = ['apple','banana','mango'];
+fruits = ['cost',10];
+let cars : string[] = ['Honda','Faw','BMW'];
+let numberArray : number[] = [1,2,3,4];
