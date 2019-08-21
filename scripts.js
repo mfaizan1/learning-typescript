90eb0630b1aa5f2640ea75e2ff894dccfeac272c
mfaizan1 <mfaizanulhaq1@gmail.com>
2019-08-22 00:04:19 +0500

    Enums

--- scripts.js
+++ scripts.js
@@ -8,3 +8,14 @@ fruits = ['cost', 10];
 var cars = ['Honda', 'Faw', 'BMW'];
 var numberArray = [1, 2, 3, 4];
 var addressTuple = ["thisstreet", 99];
+//if dont assign values to variables, number values will be assigned
+//if you give an arbitary number values to any of the variable next will 
+//follow that sequence
+var colors;
+(function (colors) {
+    colors["grey"] = "grey";
+    colors["black"] = "black";
+    colors["green"] = "green";
+})(colors || (colors = {}));
+var myColor = colors.black;
+console.log(myColor);
--- scripts.ts
+++ scripts.ts
@@ -9,3 +9,13 @@ let cars : string[] = ['Honda','Faw','BMW'];
 let numberArray : number[] = [1,2,3,4];
 
 let addressTuple: [string, number] = ["thisstreet", 99];
+//if dont assign values to variables, number values will be assigned
+//if you give an arbitary number values to any of the variable next will 
+//follow that sequence
+enum colors {
+  grey = "grey",
+  black = "black",
+  green = "green"
+}
+let myColor : colors = colors.black;
+console.log(myColor);
