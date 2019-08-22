8cfb6ae9df96c14f95474548ec0da11d257717da
mfaizan1 <mfaizanulhaq1@gmail.com>
2019-08-23 00:25:00 +0500

    rest and spread opertators

--- TSandES6/scripts.js
+++ TSandES6/scripts.js
@@ -1,3 +1,14 @@
 "use strict";
 var add = function (val1, val2) { return val1 + val2; };
 console.log(add(5, 10));
+//spread
+var numbers = [1, 4, 99, 222, 100];
+console.log(Math.max.apply(Math, numbers));
+var makeArray = function () {
+    var args = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        args[_i] = arguments[_i];
+    }
+    return args;
+};
+console.log(makeArray(1, 2, 4, 5, 6));
--- TSandES6/scripts.ts
+++ TSandES6/scripts.ts
@@ -1,3 +1,10 @@
 const add = (val1: number, val2: number):number => val1 + val2
+console.log(add(5,10));
 
-console.log(add(5,10))
+//spread
+let numbers = [1,4,99,222,100];
+console.log(Math.max(...numbers));
+let makeArray = (...args: number[]) : number[] => {
+    return args
+}
+console.log(makeArray(1,2,4,5,6))
