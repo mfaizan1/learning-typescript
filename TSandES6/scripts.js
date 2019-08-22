cdd208feb00038ebcd02afe36a5ab984bcaaea2b
mfaizan1 <mfaizanulhaq1@gmail.com>
2019-08-22 18:13:55 +0500

    arrow functions

---
+++ TSandES6/scripts.js
@@ -0,0 +1,3 @@
+"use strict";
+var add = function (val1, val2) { return val1 + val2; };
+console.log(add(5, 10));
---
+++ TSandES6/scripts.ts
@@ -0,0 +1,3 @@
+const add = (val1: number, val2: number):number => val1 + val2
+
+console.log(add(5,10))
