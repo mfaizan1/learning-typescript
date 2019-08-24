250effdd5e13ea7d4db365af540cbf993948b99b
mfaizan1 <mfaizanulhaq1@gmail.com>
2019-08-25 02:41:37 +0500

    getter setters

--- classesAndObjects/scripts.js
+++ classesAndObjects/scripts.js
@@ -2,9 +2,28 @@
 var Person = /** @class */ (function () {
     function Person(name, username) {
         this.username = username;
+        this.type = "";
+        this.age = 0;
         this.name = name;
     }
+    Person.prototype.getAge = function () {
+        return this.age;
+    };
+    Person.prototype.setAge = function (age) {
+        this.age = age;
+    };
+    Person.prototype.getType = function () {
+        return this.type;
+    };
+    Person.prototype.setType = function (type) {
+        this.type = type;
+    };
     return Person;
 }());
 var person = new Person("faizan", "fazi");
 console.log(person.name, person.username);
+console.log(person.getAge());
+person.setAge(24);
+console.log(person.getAge());
+person.setType("wierd");
+console.log(person.getType());
--- classesAndObjects/scripts.ts
+++ classesAndObjects/scripts.ts
@@ -1,10 +1,27 @@
 class Person{
     name: string;
-    private type: string;
-    protected age: number;
+    private type: string = "";
+    protected age: number = 0;
     constructor(name: string, public username: string){
         this.name = name;
+    }
+    getAge(): number{
+        return this.age;
+    }
+    setAge(age: number): void{
+        this.age = age
+    }
+    getType(): string{
+        return this.type;
+    }
+    setType(type: string): void{
+        this.type = type;
     }
 }
 const person = new Person("faizan","fazi");
 console.log(person.name, person.username);
+console.log(person.getAge());
+person.setAge(24);
+console.log(person.getAge());
+person.setType("wierd");
+console.log(person.getType());
