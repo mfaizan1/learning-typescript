bb322387e2abc72bbbbea8f90c3410fed1fc51e0
mfaizan1 <mfaizanulhaq1@gmail.com>
2019-08-27 16:27:36 +0500

    builtin generics

--- generics/scripts.js
+++ generics/scripts.js
@@ -1,7 +1,19 @@
 "use strict";
+//simple generic
 function echo(data) {
     return data;
 }
 console.log(echo('123').length);
 console.log(echo(123).length);
 console.log(echo({ data: '123' }).length);
+//better generic
+function betterEcho(data) {
+    return data;
+}
+console.log(betterEcho('123').length);
+console.log(betterEcho(123));
+console.log(betterEcho({ data: '123' }));
+var arr = [1, 4, 2, 5, 22];
+arr.push(-222);
+// arr.push('111') cannot do this now since we have made array not generic
+console.log(arr);
--- generics/scripts.ts
+++ generics/scripts.ts
@@ -15,3 +15,8 @@ console.log(betterEcho('123').length);
 console.log(betterEcho<number>(123));
 console.log(betterEcho({data: '123'}));
 
+const arr :  Array<number> = [1,4,2,5,22];
+arr.push(-222);
+// arr.push('111') cannot do this now since we have made array not generic
+console.log(arr);
+
