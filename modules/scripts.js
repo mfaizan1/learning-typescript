bcd20c3cb5dca6d09e7e542b4a475d3d65a6c844
mfaizan1 <mfaizanulhaq1@gmail.com>
2019-08-31 06:04:46 +0500

    modues importing

--- modules/scripts.js
+++ modules/scripts.js
@@ -4,4 +4,4 @@ var circle_1 = require("./circle");
 var rectangle_1 = require("./rectangle");
 console.log(circle_1.PI);
 console.log(circle_1.calcCircumference(5));
-console.log(rectangle_1.calculateRectangle(3, 5));
+console.log(rectangle_1.calculateRectangle(20, 50));
--- modules/scripts.ts
+++ modules/scripts.ts
@@ -3,4 +3,4 @@ import {calculateRectangle} from './rectangle';
 
 console.log(PI);
 console.log(calcCircumference(5));
-console.log(calculateRectangle(3,5));
+console.log(calculateRectangle(20,50));
